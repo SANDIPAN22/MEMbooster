a0b71b65a81dbaa2e3ae0f129a90090523ee9c88
Sandipan Chakraborty <chak.sandipan22@gmail.com>
2024-05-18 19:22:03 +0530

    Routing + Suspense fallback + Lazy loading

--- frontend-membooster/src/App.tsx
+++ frontend-membooster/src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
-import Button from '@mui/material/Button'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Suspense, lazy } from 'react'
+const Login = lazy(() =>  import('./pages/Login'))
+const SignIn = lazy(()=> import('./pages/SignIn'))
+const Dashboard = lazy(()=> import('./pages/Dashboard'))
+const NewNote = lazy(()=> import('./pages/NewNote'))
+import Loader from './components/Loader'
+
 const App = () => {
   return (
-    <div>App <Button> Kick Me</Button></div>
+    <Suspense fallback={<Loader/>}>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login/>}> </Route>
+          <Route path='/signin' element={<SignIn/>}></Route>
+          <Route path= '/' element={<Dashboard/>}></Route>
+          <Route path='/new' element={<NewNote/>}></Route>
+          <Route path='*' element={<Navigate to="/"/>}></Route>
+        </Routes>
+
+      </Router>
+    </Suspense>
   )
 }
 
---
+++ frontend-membooster/src/components/Loader.tsx
@@ -0,0 +1,24 @@
+import { Box, styled } from '@mui/material'
+import { InfinitySpin } from 'react-loader-spinner'
+
+const MyCenteredBox = styled(Box)({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+})
+
+const Loader = () => {
+  return (
+    <MyCenteredBox>
+    <InfinitySpin
+    // visible={1}
+    width="200"
+    color="#070707"
+    // ariaLabel="infinity-spin-loading"
+    />
+    </MyCenteredBox>
+  )
+}
+
+export default Loader
