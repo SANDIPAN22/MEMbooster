9fb16d188688002eb0b9c7fe3b69b886438d54f0
Sandipan Chakraborty <chak.sandipan22@gmail.com>
2024-05-29 01:13:00 +0530

    Upgarded Express JS setup

--- backend-membooster/src/config/index.ts
+++ backend-membooster/src/config/index.ts
@@ -6,3 +6,4 @@ dotenv.config()
 
 export const PORT: number = parseInt(process.env.PORT || "", 10)
 export const NODE_ENV : string = process.env.NODE_ENV || "development"
+export const MORGAN_MODE: string = process.env.MORGAN_MODE || "dev"
--- backend-membooster/src/server.ts
+++ backend-membooster/src/server.ts
@@ -1,8 +1,24 @@
 import express from "express"
 import type {Express, Request, Response} from "express"
-import { PORT, NODE_ENV } from "./config"
+import { PORT, NODE_ENV, MORGAN_MODE } from "./config"
+import helmet from "helmet"
+import cors from "cors"
+import morgan from "morgan"
 
 const app : Express = express()
+
+// If in the server the app runs behind the proxies then setting the below will be helpful to debug
+app.enable("trust proxy")
+
+// middlewares setup time brother
+app.use(helmet({
+    contentSecurityPolicy: false
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())  // this one is very risky it is allowing everyone, you need to white-list only necessary origins
+app.use(morgan('MORGAN_MODE'))
+
 
 app.get('/', (req: Request, res: Response)=> {
     res.send("Hello")
