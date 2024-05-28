e4d773423e6d7b92214295a7f894b71e4caba093
Sandipan Chakraborty <chak.sandipan22@gmail.com>
2024-05-29 02:18:59 +0530

    Implementing pro code consistency

--- backend-membooster/src/config/index.ts
+++ backend-membooster/src/config/index.ts
@@ -1,9 +1,8 @@
 // we write generic configs (which are not any thing specific) here in the index.ts
 
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-
-export const PORT: number = parseInt(process.env.PORT || "", 10)
-export const NODE_ENV : string = process.env.NODE_ENV || "development"
-export const MORGAN_MODE: string = process.env.MORGAN_MODE || "dev"
+export const PORT: number = parseInt(process.env.PORT || "", 10);
+export const NODE_ENV: string = process.env.NODE_ENV || "development";
+export const MORGAN_MODE: string = process.env.MORGAN_MODE || "dev";
--- backend-membooster/src/server.ts
+++ backend-membooster/src/server.ts
@@ -1,29 +1,32 @@
-import express from "express"
-import type {Express, Request, Response} from "express"
-import { PORT, NODE_ENV, MORGAN_MODE } from "./config"
-import helmet from "helmet"
-import cors from "cors"
-import morgan from "morgan"
+import express from "express";
+import type { Express, Request, Response } from "express";
+import { PORT, NODE_ENV, MORGAN_MODE } from "./config";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
-const app : Express = express()
+const app: Express = express();
 
 // If in the server the app runs behind the proxies then setting the below will be helpful to debug
-app.enable("trust proxy")
+app.enable("trust proxy");
 
 // middlewares setup time brother
-app.use(helmet({
-    contentSecurityPolicy: false
-}))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cors())  // this one is very risky it is allowing everyone, you need to white-list only necessary origins
-app.use(morgan('MORGAN_MODE'))
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors()); // this one is very risky it is allowing everyone, you need to white-list only necessary origins
+app.use(morgan(MORGAN_MODE));
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
 
-app.get('/', (req: Request, res: Response)=> {
-    res.send("Hello")
-})
-
-app.listen(PORT, ()=> {
-    console.log(`The Server is Up and Running at port ${PORT} in ${NODE_ENV} mode.`)
-})
+app.listen(PORT, () => {
+  console.log(
+    `The Server is Up and Running at port ${PORT} in ${NODE_ENV} mode.`,
+  );
+});
